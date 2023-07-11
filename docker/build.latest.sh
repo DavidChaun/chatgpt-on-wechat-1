de97225e35a617f82c17ac1caa456e966b55ea34
chengzhiwei <chengzhiwei@datastory.com.cn>
2023-07-11 16:39:55 +0800

    feat: me for docker file.

--- docker/build.latest.sh
+++ docker/build.latest.sh
@@ -3,6 +3,6 @@
 unset KUBECONFIG
 
 cd .. && docker build -f docker/Dockerfile.latest \
-             -t zhayujie/chatgpt-on-wechat .
+             -t wei/chatgpt-on-wechat .
 
-docker tag zhayujie/chatgpt-on-wechat zhayujie/chatgpt-on-wechat:$(date +%y%m%d)
+docker tag wei/chatgpt-on-wechat wei/chatgpt-on-wechat:$(date +%y%m%d)
