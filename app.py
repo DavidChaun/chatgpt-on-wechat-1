52f73b2015c3aa24c755f5dd986dd3a43bd5faf8
chengzhiwei <chengzhiwei@datastory.com.cn>
2023-07-28 10:30:22 +0800

    feat: 不需要验证http证书。

--- app.py
+++ app.py
@@ -52,7 +52,11 @@ def login(access: str):
 
     if not qr_url:
         return {"code": 500, "msg": "no qr"}
-    response = requests.get(qr_url, headers={"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"})
+    response = requests.get(
+        url=qr_url,
+        headers={"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"},
+        verify=False
+    )
     _byte = response.content
 
     return Response(content=_byte)
